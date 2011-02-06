f3b41e5f899c5aa8eb49659ca6fc83c3996e86c1
Michael Tremer <michael.tremer@ipfire.org>
2011-02-06 22:31:13 +0100

    plugins: Add possibiblity to register and run a plugin.

--- pakfire/plugins/__init__.py
+++ pakfire/plugins/__init__.py
@@ -10,9 +10,21 @@ class Plugins(object):
 
 		self.__plugins = []
 
-	def run(self, method):
+	def register_plugin(self, plugin):
+		# Create instance of plugin
+		plugin = plugin(self.pakfire)
+
+		self.__plugins.append(plugin)
+
+	def run(self, method, *args, **kwargs):
 		if not method in self.allowed_methods:
 			raise Exception, "Unallowed method called '%s'" % method
 
 		logging.debug("Running plugin method '%s'" % method)
 
+		for plugin in self.__plugins:
+			func = getattr(plugin, method, None)
+			if not func:
+				continue
+
+			func(*args, **kwargs)
