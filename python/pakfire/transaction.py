93517d4b62dfc9af5deaee37c25992a653e71b3a
Michael Tremer <michael.tremer@ipfire.org>
2012-03-23 16:42:50 +0100

    Fix two unallowed language strings.

--- python/pakfire/transaction.py
+++ python/pakfire/transaction.py
@@ -92,14 +92,14 @@ class TransactionCheck(object):
 
 			if len(files) == 2:
 				logger.critical(
-					_("file %s from %s conflicts with file from package %s") % \
-						(name, pkgs[0], pkgs[1])
+					_("file %(name)s from %(pkg1)s conflicts with file from package %(pkg2)s") % \
+						{ "name" : name, "pkg1" : pkgs[0], "pkg2" : pkgs[1] })
 				)
 
 			elif len(files) >= 3:
 				logger.critical(
-					_("file %s from %s conflicts with files from %s") % \
-						(name, pkgs[0], i18n.list(pkgs[1:]))
+					_("file %(name)s from %(pkg)s conflicts with files from %(pkgs)s") % \
+						{ "name" : name, "pkg" : pkgs[0], "pkgs" : i18n.list(pkgs[1:])})
 				)
 
 		for mp in self.mountpoints:
