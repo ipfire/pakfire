e1392437178dbd4fdc0c4b084d94d17194f1f8e6
Michael Tremer <michael.tremer@ipfire.org>
2019-06-10 16:39:59 +0100

    tests: libpakfire: Test pakfire_basename/dirname
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

---
+++ tests/libpakfire/util.c
@@ -0,0 +1,57 @@
+/*#############################################################################
+#                                                                             #
+# Pakfire - The IPFire package management system                              #
+# Copyright (C) 2019 Pakfire development team                                 #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+#############################################################################*/
+
+#include <string.h>
+
+#include <pakfire/parser.h>
+#include <pakfire/util.h>
+
+#include "../testsuite.h"
+
+int test_basename(const test_t* t) {
+	const char* dir = "/a/b/c";
+
+	char* output = pakfire_basename(dir);
+	assert_compare(output, "c", EXIT_FAILURE);
+	pakfire_free(output);
+
+	return EXIT_SUCCESS;
+}
+
+int test_dirname(const test_t* t) {
+	const char* dir = "/a/b/c";
+
+	char* output = pakfire_dirname(dir);
+	assert_compare(output, "/a/b", EXIT_FAILURE);
+	pakfire_free(output);
+
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+	testsuite_init();
+
+	testsuite_t* ts = testsuite_create(2);
+
+	testsuite_add_test(ts, "test_basename", test_basename);
+	testsuite_add_test(ts, "test_dirname", test_dirname);
+
+	return testsuite_run(ts);
+}
