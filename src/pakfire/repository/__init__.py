5ea5ae05ec905e88eec7d6a4c5f102ea6ab6dfc5
Michael Tremer <michael.tremer@ipfire.org>
2021-01-09 17:20:58 +0000

    repo: Make sure that arch is a string
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/pakfire/repository/__init__.py
+++ src/pakfire/repository/__init__.py
@@ -107,7 +107,7 @@ class Repositories(object):
 		# Handle variable expansion.
 		replaces = {
 			"name" : name,
-			"arch" : self.pakfire.arch,
+			"arch" : "%s" % self.pakfire.arch,
 		}
 
 		for k, v in list(_args.items()):
